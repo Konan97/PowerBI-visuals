e398411aec6d7ba3583b572d488b60a2597c4fdf
Sun <ysun98@volvocars.com>
2025-09-04 13:56:21 -0400

    add GUI

--- KDP_BIS/KDP_BIS.py
+++ KDP_BIS/KDP_BIS.py
@@ -8,120 +8,119 @@ from datetime import datetime, timedelta
 from snowflake.snowpark import Session
 import tomllib
 
-def getConnection():
-    connections = {}
-    try:
-        with open('connections.toml', 'rb') as f:
-            connections = tomllib.load(f)
-        print(connections)
-    except FileNotFoundError:
-        print("connections.toml file not found.")
+class Comparison(object):
+    def __init__(self, directory_path, user_input):
+        self.directory_path = directory_path
+        self.user_input = user_input
+        self.output_columns = ['Item', 'Item Type', 'Version', 'Model', 'No', 'Type', 'Text',
+        'Base file', 'Rel pos', 'Length', 'Nr.', 'Cd', 'Condition',
+        'Descr. PML', 'Neg', 'Base file condition', 'Pop Base file',
+        'Starting position', 'Length.1', 'Valid from', 'Valid to',
+        'Plass.week from', 'To plass.week', 'Shift in', 'Shift out']
+        
+        self.SW_partNumOrder = {'SWS1': 27, 'SWE1': 39, 'SWLM': 75, 'SWL2': 84, 'SWL3': 93, 'SWL4': 102, 'SWP2': 138, 'SWM1': 30,
+        'SWTV': 12, 'SWZ1': 17, 'SWP1': 129, 'SWBL': 21, 'SWBT': 48, 'SWFD': 183, 'SWCE': 66, 'SWP3': 147,
+        'SWP4': 156, 'SWK1': 12, 'SWK2': 12, 'SWK3': 12, 'SWK4': 12, 'SWK5': 12, 'SWK6': 12,
+        'SWP5': 165}
 
-    connection_parameters = {
-        "account": connections['snowflake_account']['account'],
-        "user": connections['snowflake_account']['user'],
-        "role": connections['snowflake_account']['role'],
-        "database": connections['snowflake_account']['database'],
-        "schema": connections['snowflake_account']['schema'],
-        "warehouse": connections['snowflake_account']['warehouse'],
-        "authenticator": connections['snowflake_account']['authenticator']
-    }
-    session = Session.builder.configs(connection_parameters).create()
-    return session
+    def getConnection(self, user_input):
+        connections = {}
+        try:
+            with open('connections.toml', 'rb') as f:
+                connections = tomllib.load(f)
+            print(connections)
+        except FileNotFoundError:
+            print("connections.toml file not found.")
 
-def getTable(session: snowpark.Session):
-    tableName = 'MANUFACTURING_ENTERPRISE_DATA_PRODUCTS.BIS_ITEMS.BIS_ITEM_DETAILS'
-    # Filter for informative columns and SN read data rows
-    filter_cols = ['"_id_site"', '"bartender_xml_identifier"', '"bis_item"', '"version"', '"model"',
-                   '"text_type"', '"text_steering"', '"relative_position"', 
-                   '"bis_item_steering_text_length"']
-    dataframe = session.table(tableName)\
-        .filter(
-            #(col('"Attribute"').like('%SN SWDL%')) &
-            (col('"bis_item"').isNotNull()) &
-            (col('"bartender_xml_identifier"').like('%$%')) &
-            (col('"_id_site"') == 'VCCH')
-        ).select(filter_cols)
- 
-    # convert to Pandas (modin) for analysis 
-    data = dataframe.to_pandas()
+        connection_parameters = {
+            "account": connections['snowflake_account']['account'],
+            "user": user_input,
+            "role": connections['snowflake_account']['role'],
+            "database": connections['snowflake_account']['database'],
+            "schema": connections['snowflake_account']['schema'],
+            "warehouse": connections['snowflake_account']['warehouse'],
+            "authenticator": connections['snowflake_account']['authenticator']
+        }
+        session = Session.builder.configs(connection_parameters).create()
+        return session
+
+    def getTable(self, session: snowpark.Session):
+        tableName = 'MANUFACTURING_ENTERPRISE_DATA_PRODUCTS.BIS_ITEMS.BIS_ITEM_DETAILS'
+        # Filter for informative columns and SN read data rows
+        filter_cols = ['"_id_site"', '"bartender_xml_identifier"', '"bis_item"', '"version"', '"model"',
+                    '"text_type"', '"text_steering"', '"relative_position"', 
+                    '"bis_item_steering_text_length"']
+        dataframe = session.table(tableName)\
+            .filter(
+                #(col('"Attribute"').like('%SN SWDL%')) &
+                (col('"bis_item"').isNotNull()) &
+                (col('"bartender_xml_identifier"').like('%$%')) &
+                (col('"_id_site"') == 'VCCH')
+            ).select(filter_cols)
     
-    return data
+        # convert to Pandas (modin) for analysis 
+        data = dataframe.to_pandas()
+        return data
 
 
 
-def KDP_from_csv(directory_path):
-    """Helper function to read a CSV file and return a DataFrame."""
-    
-    KDP_df = pd.read_excel(directory_path, skiprows=8)
-    # KDP_to_BIS = {'Part Number': 'Text', 'Part type': 'Rel pos', 'ECU': 'bartender_xml_identifier', 'bartender_xml_identifier': 'bis_item'}
-    # modify KDP columns
-    KDP_df = KDP_df[~KDP_df['Part Number'].isin([32218512, 32375204])]  # remove rows with Part Number 32218512 or 32375204
-    KDP_df['Part Number'] = KDP_df['Part Number'].astype(str)
-    
+    def KDP_from_csv(self, directory_path):
+        """Helper function to read a CSV file and return a DataFrame."""
+        
+        KDP_df = pd.read_excel(directory_path, skiprows=8)
+        # KDP_to_BIS = {'Part Number': 'Text', 'Part type': 'Rel pos', 'ECU': 'bartender_xml_identifier', 'bartender_xml_identifier': 'bis_item'}
+        # modify KDP columns
+        KDP_df = KDP_df[~KDP_df['Part Number'].isin([32218512, 32375204])]  # remove rows with Part Number 32218512 or 32375204
+        KDP_df['Part Number'] = KDP_df['Part Number'].astype(str)
 
-    return KDP_df
+        return KDP_df
 
-def BIS_from_snowflake():
-    session = getConnection()
-    BIS_df = getTable(session)
-    BIS_df['text_steering'] = BIS_df['text_steering'].str.replace(' ', '')
-    session.close()
-    return BIS_df
+    def BIS_from_snowflake(self, user_input):
+        session = self.getConnection(user_input)
+        BIS_df = self.getTable(session)
+        BIS_df['text_steering'] = BIS_df['text_steering'].str.replace(' ', '')
+        session.close()
+        return BIS_df
 
-def create_new_row(row):
-    # Mapping dictionary from KDP to BIS
-    KDP_to_BIS = {'Part Number': 'Text', 'Part type': 'Rel pos', 'ECU': 'bartender_xml_identifier', 'bartender_xml_identifier': 'bis_item'}
-    new_row = {'Version': 1, 'Model': 7, 'Neg': False, 'Base file condition': '', 'Pop Base file': '',
-       'Starting position': 0, 'Length.1': 0, 'Valid from': '2003181 05:15:00', 'Valid to': '9999525 23:59:59',
-       'Plass.week from': 190001, 'To plass.week': 210001, 'Shift in': '', 'Shift out': ''}
-    for col_name in KDP_df.columns: # Iterate through original KDP columns to check mapping
-        if col_name in KDP_to_BIS:
-        # Access the value from the current row being processed by apply
-            col_value = row[col_name]
-        # print(col_value)
-            if col_value in SW_partNumOrder:
-                new_row[KDP_to_BIS[col_name]] = SW_partNumOrder[col_value]
-            elif col_name == 'ECU':
-                # Need to handle potential NaN values in 'ECU' column
-                if pd.notna(col_value):
-                    # Use a conditional check to avoid error if the key doesn't exist
-                    new_row['ECU'] = col_value
-                    if "$" + str(col_value) in BIS_df['bartender_xml_identifier'].values:
-                        new_row[KDP_to_BIS[KDP_to_BIS[col_name]]] = BIS_df[BIS_df['bartender_xml_identifier'] == "$" + str(col_value)]['bis_item'].iloc[0]
+    def create_new_row(self, row):
+        # Mapping dictionary from KDP to BIS
+        KDP_to_BIS = {'Part Number': 'Text', 'Part type': 'Rel pos', 'ECU': 'bartender_xml_identifier', 'bartender_xml_identifier': 'bis_item'}
+        new_row = {'Version': 1, 'Model': 7, 'Neg': False, 'Base file condition': '', 'Pop Base file': '',
+        'Starting position': 0, 'Length.1': 0, 'Plass.week from': 190001, 'To plass.week': 210001, 'Shift in': '', 'Shift out': ''}
+        for col_name in self.KDP_df.columns: # Iterate through original KDP columns to check mapping
+            if col_name in KDP_to_BIS:
+            # Access the value from the current row being processed by apply
+                col_value = row[col_name]
+            # print(col_value)
+                if col_value in self.SW_partNumOrder:
+                    new_row[KDP_to_BIS[col_name]] = self.SW_partNumOrder[col_value]
+                elif col_name == 'ECU':
+                    # Need to handle potential NaN values in 'ECU' column
+                    if pd.notna(col_value):
+                        # Use a conditional check to avoid error if the key doesn't exist
+                        new_row['ECU'] = col_value
+                        if "$" + str(col_value) in self.BIS_df['bartender_xml_identifier'].values:
+                            new_row[KDP_to_BIS[KDP_to_BIS[col_name]]] = self.BIS_df[self.BIS_df['bartender_xml_identifier'] == "$" + str(col_value)]['bis_item'].iloc[0]
+                        else:
+                            print(f"Warning: bartender_xml_identifier for ECU '{col_value}' not found in BIS_df.")
                     else:
-                        print(f"Warning: bartender_xml_identifier for ECU '{col_value}' not found in BIS_df.")
+                        print("Warning: NaN value in ECU column, cannot map to bartender_xml_identifier.")
                 else:
-                    print("Warning: NaN value in ECU column, cannot map to bartender_xml_identifier.")
-            else:
-                new_row[KDP_to_BIS[col_name]] = col_value
+                    new_row[KDP_to_BIS[col_name]] = col_value
 
-    return new_row
+        return new_row
 
-if __name__ == "__main__":
-    output_columns = ['Item', 'Item Type', 'Version', 'Model', 'No', 'Type', 'Text',
-       'Base file', 'Rel pos', 'Length', 'Nr.', 'Cd', 'Condition',
-       'Descr. PML', 'Neg', 'Base file condition', 'Pop Base file',
-       'Starting position', 'Length.1', 'Valid from', 'Valid to',
-       'Plass.week from', 'To plass.week', 'Shift in', 'Shift out']
-    
-    SW_partNumOrder = {'SWS1': 27, 'SWE1': 39, 'SWLM': 75, 'SWL2': 84, 'SWL3': 93, 'SWL4': 102, 'SWP2': 138, 'SWM1': 30,
-       'SWTV': 12, 'SWZ1': 17, 'SWP1': 129, 'SWBL': 21, 'SWBT': 48, 'SWFD': 183, 'SWCE': 66, 'SWP3': 147,
-      'SWP4': 156, 'SWK1': 12, 'SWK2': 12, 'SWK3': 12, 'SWK4': 12, 'SWK5': 12, 'SWK6': 12,
-       'SWP5': 165}
-    
-    # imported KDP file
-    
-    directory_path = "C:\\Users\\YSUN98\\OneDrive - Volvo Cars\\Desktop\\KDP_BIS\\Preserie-725B_PP11CH.xlsx"
-    KDP_df = KDP_from_csv(directory_path)
-    BIS_df = BIS_from_snowflake()
-    # Process
-    # 'Part Number' from the filtered KDP dataframe is not in the 'text_steering' column of the BIS dataframe.
-    temp = KDP_df[~KDP_df.apply(lambda row: row['Part Number'] in BIS_df[BIS_df['bartender_xml_identifier'] == "$" + str(row['ECU'])]['text_steering'].values, axis=1)]
-    print(temp.head(20))
-    new_rows_list = temp.apply(lambda row: create_new_row(row), axis=1).tolist()
-    new_rows_df = pd.DataFrame(new_rows_list)
-    print(new_rows_df.head(20))
+    def run_process(self, directory_path, user_input):
+        # imported KDP file
+        KDP_df = self.KDP_from_csv(directory_path)
+        BIS_df = self.BIS_from_snowflake(user_input)
+        # Process
+        # 'Part Number' from the filtered KDP dataframe is not in the 'text_steering' column of the BIS dataframe.
+        temp = KDP_df[~KDP_df.apply(lambda row: row['Part Number'] in BIS_df[BIS_df['bartender_xml_identifier'] == "$" + str(row['ECU'])]['text_steering'].values, axis=1)]
+        print(temp.head(20))
+        new_rows_list = temp.apply(lambda row: self.create_new_row(row), axis=1).tolist()
+        new_rows_df = pd.DataFrame(new_rows_list)
+        print(new_rows_df.head(20))
 
-    # output to CSV
-    new_rows_df.to_csv("C:\\Users\\YSUN98\\OneDrive - Volvo Cars\\Desktop\\KDP_BIS\\BIS_export_new.csv")
+        # output to CSV
+        new_rows_df.to_csv("C:\\Users\\YSUN98\\OneDrive - Volvo Cars\\Desktop\\KDP_BIS\\BIS_export_725B_PP12CH.csv")
---
+++ KDP_BIS/app_GUI.py
@@ -0,0 +1,92 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QMainWindow, 
+                             QFormLayout, QLineEdit, QLabel, QVBoxLayout)
+from PyQt5.QtCore import Qt
+import KDP_BIS
+
+# Only needed for access to command line arguments
+import sys
+
+class DropLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.setText("\n\n Drop KDP Excel File Here \n\n")
+        self.setStyleSheet("QLabel{border: 4px dashed #aaa;}")
+        self.setAlignment(Qt.AlignCenter)
+        self.setAcceptDrops(True)
+        self.file_path = None
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    # ensure your dropEvent() method does not return any value
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+            file_path = event.mimeData().urls()[0].toLocalFile()
+            self.setText(f"File dropped: {file_path}")
+            if file_path.endswith((".xlsx", ".xls")):
+                self.setText(f"✅ Loaded: {file_path}")
+                self.file_path = file_path
+            else:
+                self.setText("❌ Not an Excel file")
+        else:
+            event.ignore()
+
+
+class MainApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("KDP to BIS Comparator")
+        self.setGeometry(200, 200, 600, 400)
+
+        # Main layout
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        main_layout = QVBoxLayout(central_widget)
+
+        # Snowflake Credentials form
+        self.form_layout = QFormLayout()
+        self.user_input = QLineEdit()
+        self.form_layout.addRow("Snowflake Account:", self.user_input)
+
+        # Drag and Drop area
+        self.drop_label = DropLabel()
+        
+        
+        # Process Button
+        self.process_button = QPushButton("Run Comparison")
+        self.process_button.clicked.connect(self.run_comparison)
+
+        # Status Bar
+        self.status_bar = self.statusBar()
+        self.setStatusBar(self.status_bar)
+
+        # Main layout assembly
+        main_layout.addWidget(self.drop_label)
+        main_layout.addLayout(self.form_layout)
+        main_layout.addWidget(self.process_button)
+    
+    def run_comparison(self):
+        try: 
+            if not self.drop_label.file_path:
+                raise ValueError("No file dropped. Please drop an Excel file.")
+            else: 
+                self.kdp_file_path = self.drop_label.file_path
+                KDP_BIS.run_process(self.kdp_file_path, self.user_input.text())
+                self.statusBar().showMessage("Process completed successfully.")
+        except Exception as e:
+            self.statusBar().showMessage(str(e))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Create a Qt widget, which will be our window.
+    window = MainApp()
+    window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+    # Start the event loop.
+    app.exec()
